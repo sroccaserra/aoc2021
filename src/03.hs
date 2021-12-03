ab94a5064d997f96de1b3d3abb00a3f1f9177551
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-12-03 12:13:44 +0100

    [day 03] Solve part two (Haskell)

--- src/03.hs
+++ src/03.hs
@@ -1,12 +1,12 @@
 import Data.List
-import Text.ParserCombinators.ReadP
 
-import Common (getDayRawInput)
+import Common (getDayLines)
 
 main = do
-  input <- lines <$> getDayRawInput 3
+  input <- getDayLines 3
   let matrix = map (read . (:[])) <$> input
   print $ partOne matrix
+  print $ partTwo matrix
 
 partOne matrix = toDec gamma * toDec epsilon
   where nbColumns = length $ head matrix
@@ -14,15 +14,30 @@ partOne matrix = toDec gamma * toDec epsilon
         gamma = map mostFrequent stats
         epsilon = map flipNumber gamma
 
-colStat matrix n = foldl incStat (0, 0) $ col n matrix
+partTwo matrix = toDec o2 * toDec co2
+  where nbColumns = length $ head matrix
+        o2 = head $ foldl stepO2 matrix [0..nbColumns-1]
+        co2 = head $ foldl stepCO2 matrix [0..nbColumns-1]
+
+colStat matrix n = foldl incStat (0, 0) $ map (!! n) matrix
 
 incStat (x, y) 0 = (x + 1, y)
 incStat (x, y) 1 = (x, y + 1)
-
-col n = map (!! n)
 
 mostFrequent (nbZeros, nbOnes) = if nbOnes >= nbZeros then 1 else 0
 
 flipNumber c = if 0 == c then 1 else 0
 
 toDec = foldl1' $ (+) . (*2)
+
+stepO2 [lastRow] _ = [lastRow]
+stepO2 matrix n = filter ((b==).(!!n)) matrix
+  where nbColumns = length $ head matrix
+        stats = map (colStat matrix) [0..nbColumns-1]
+        b = mostFrequent $ stats !! n
+
+stepCO2 [lastRow] _ = [lastRow]
+stepCO2 matrix n = filter ((b==).(!!n)) matrix
+  where nbColumns = length $ head matrix
+        stats = map (colStat matrix) [0..nbColumns-1]
+        b = flipNumber $ mostFrequent $ stats !! n
