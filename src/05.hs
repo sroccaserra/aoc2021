e61b117135369cdf4b2a2a4ffcfde7a8c6c796c4
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-12-05 18:54:37 +0100

    :recycle: Replace bresenham by a pointsBetween function

--- src/05.hs
+++ src/05.hs
@@ -1,19 +1,24 @@
 import Data.List
 import Text.ParserCombinators.ReadP
 
-import Common (getParsedLines, Coord(..), unsigned, bresenham)
+import Common (getParsedLines, unsigned, Coord(..))
 
 main = do
   input <- getParsedLines 5 parser
   print $ partOne input
   print $ partTwo input
 
-partOne = partTwo . filter isHorizontalOrVertical
+partOne = partTwo . filter isHorizOrVert
+  where isHorizOrVert (Coord x1 y1, Coord x2 y2) = x1 == x2 || y1 == y2
 
-partTwo :: [(Coord, Coord)] -> Int
-partTwo = length . filter ((> 1) . length) . group . sort . concatMap (uncurry bresenham)
+partTwo = length . filter ((> 1) . length) . group . sort . concatMap (uncurry pointsBetween)
 
-isHorizontalOrVertical (Coord x1 y1, Coord x2 y2) = x1 == x2 || y1 == y2
+pointsBetween p@(Coord x1 y1) (Coord x2 y2) = take (n+1) (iterate inc p)
+  where (dx, dy) = (x2 - x1, y2 - y1)
+        incX = if dx == 0 then 0 else if dx > 0 then 1 else -1
+        incY = if dy == 0 then 0 else if dy > 0 then 1 else -1
+        n = max (abs dx) (abs dy)
+        inc (Coord x y) = Coord (x + incX) (y + incY)
 
 parser = (,) <$> coordParser <* string " -> " <*> coordParser
 coordParser = Coord <$> unsigned <* char ',' <*> unsigned
