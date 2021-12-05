305d682f5aee71d18d4daab55dd9ca0991d6b212
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-12-05 18:02:38 +0100

    [day 05] Solve part one

--- src/05.hs
+++ src/05.hs
@@ -5,10 +5,15 @@ import Common (getParsedLines, Coord(..), unsigned, bresenham)
 
 main = do
   input <- getParsedLines 5 parser
+  print $ partOne input
   print $ partTwo input
+
+partOne = partTwo . filter isHorizontalOrVertical
 
 partTwo :: [(Coord, Coord)] -> Int
 partTwo = length . filter ((> 1) . length) . group . sort . concatMap (uncurry bresenham)
+
+isHorizontalOrVertical (Coord x1 y1, Coord x2 y2) = x1 == x2 || y1 == y2
 
 parser = (,) <$> coordParser <* string " -> " <*> coordParser
 coordParser = Coord <$> unsigned <* char ',' <*> unsigned
