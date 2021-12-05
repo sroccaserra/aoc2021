3be1296e6ffa5d1c72d7f171fd4e1273e64682ce
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-12-05 16:09:22 +0100

    :recycle: Rename coords -> pointPair

--- src/05.py
+++ src/05.py
@@ -3,23 +3,23 @@ import fileinput
 from collections import defaultdict
 
 
-def solve_1(coordsPairs):
+def solve_1(pointPairs):
     grid = defaultdict(int)
-    for coords in coordsPairs:
-        markLine(grid, coords)
+    for pointPair in pointPairs:
+        markLine(grid, pointPair)
     return len([k for k in grid if grid[k] > 1])
 
 
-def solve_2(coordsPairs):
+def solve_2(pointPairs):
     grid = defaultdict(int)
-    for coords in coordsPairs:
-        markLine(grid, coords, True)
+    for pointPair in pointPairs:
+        markLine(grid, pointPair, True)
     return len([k for k in grid if grid[k] > 1])
 
 
-def markLine(grid, coords, with_diagonals=False):
-    start, end = coords
-    dx, dy = end[0] - start[0], end[1] - start[1]
+def markLine(grid, pointPair, with_diagonals=False):
+    (x1, y1), (x2, y2) = pointPair
+    dx, dy = x2 - x1, y2 - y1
     if not with_diagonals and dx != 0 and dy != 0:
         return
 
@@ -27,13 +27,13 @@ def markLine(grid, coords, with_diagonals=False):
     inc_x = (1 if dx > 0 else -1) if dx != 0 else 0
     inc_y = (1 if dy > 0 else -1) if dy != 0 else 0
     for i in range(0, n+1):
-        p = (start[0]+i*inc_x, start[1]+i*inc_y)
+        p = (x1+i*inc_x, y1+i*inc_y)
         grid[p] += 1
 
 
 if __name__ == '__main__' and not sys.flags.interactive:
     lines = [line.strip() for line in fileinput.input()]
     pairs = [line.split(' -> ') for line in lines]
-    coordsPairs = [[tuple([int(w) for w in s.split(',')]) for s in pair] for pair in pairs]
-    print(solve_1(coordsPairs))
-    print(solve_2(coordsPairs))
+    pointPairs = [[tuple([int(w) for w in s.split(',')]) for s in pair] for pair in pairs]
+    print(solve_1(pointPairs))
+    print(solve_2(pointPairs))
