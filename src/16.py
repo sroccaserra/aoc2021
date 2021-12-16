273dbc715f073d892fcfe91309a7e27cd35954e5
sroccaserra <sroccaserra@yahoo.com>
2021-12-16 12:11:14 +0100

    [day 16] Solve part two

--- src/16.py
+++ src/16.py
@@ -3,14 +3,24 @@ import fileinput
 from collections import deque
 
 
-EX_0 = 'D2FE28'
-EX_1 = '38006F45291200'
-EX_2 = 'EE00D40C823060'
-EX_3 = '8A004A801A8002F478'
-EX_4 = '620080001611562C8802118E34'
-EX_5 = 'C0015000016115A2E0802F182340'
-EX_6 = 'A0016C880162017C3686B18A3D4780'
+# Part one examples
+EX_00 = 'D2FE28'
+EX_01 = '38006F45291200'
+EX_02 = 'EE00D40C823060'
+EX_03 = '8A004A801A8002F478'
+EX_04 = '620080001611562C8802118E34'
+EX_05 = 'C0015000016115A2E0802F182340'
+EX_06 = 'A0016C880162017C3686B18A3D4780'
 
+# Part two examples
+EX_07 = 'C200B40A82'
+EX_08 = '04005AC33890'
+EX_09 = '880086C3E88112'
+EX_10 = 'CE00C43D881120'
+EX_11 = 'D8005AC2A8F0'
+EX_12 = 'F600BC2D8F'
+EX_13 = '9C005AC2F8F0'
+EX_14 = '9C0141080250320F1802104A08'
 
 S = [0]
 
@@ -24,9 +34,48 @@ def solve_1(line):
     return S[0]
 
 
-def sum_versions(packets):
-    if type(packets) == int:
+def solve_2(line):
+    pc = 0
+    rom = to_01_list(line)
+    ast = []
+    state = [pc, rom, ast]
+    read_packets(state)
+    return eval_ast(ast[0])
+
+
+def eval_ast(ast):
+    (_, type_ID, data) = ast
+    if type_ID == 0:
+        result = 0
+        for operand in data:
+            result += eval_ast(operand)
+        return result
+    if type_ID == 1:
+        result = 1
+        for operand in data:
+            result *= eval_ast(operand)
+        return result
+    if type_ID == 2:
+        result = min([eval_ast(operand) for operand in data])
+        return result
+    if type_ID == 3:
+        result = max([eval_ast(operand) for operand in data])
+        return result
+    if type_ID == 4:
+            return data
+    if type_ID == 5:
+        if eval_ast(data[0]) > eval_ast(data[1]):
+            return 1
         return 0
+    if type_ID == 6:
+        if eval_ast(data[0]) < eval_ast(data[1]):
+            return 1
+        return 0
+    if type_ID == 7:
+        if eval_ast(data[0]) == eval_ast(data[1]):
+            return 1
+        return 0
+    return type_ID, data, result
 
 
 def read_packets(state):
@@ -109,6 +158,5 @@ def to_01_list(line):
 
 if __name__ == '__main__' and not sys.flags.interactive:
     line = next(fileinput.input()).strip()
-    #for ex in [EX_4]:
-    #    print(solve_1(ex))
     print(solve_1(line))
+    print(solve_2(line))
