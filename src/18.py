9f3af4ee9288af338e088ed84fd8170030f70b11
sroccaserra <sroccaserra@yahoo.com>
2021-12-18 09:42:44 +0100

    Add magnitude function, can annotate & strip trees

--- src/18.py
+++ src/18.py
@@ -12,10 +12,16 @@ VALUE = 2
 def solve_1(lines):
     sn = [[1,2],3]
     sn = [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
+    sn = [[1,2],[[3,4],5]]
     print(sn)
     print(create_map(sn))
-    augment_tree(sn)
-    return sn
+    annotate_tree(sn)
+    print(sn)
+    return magnitude(sn)
+
+
+def add(sn1, sn2):
+    return annotate_tree([sn1, sn2])
 
 
 def create_map(sn):
@@ -24,24 +30,44 @@ def create_map(sn):
     while stack:
         node, d = stack.pop()
         if type(node) == list:
-            stack.append((node[1], d+1))
-            stack.append((node[0], d+1))
+            stack.append((node[R], d+1))
+            stack.append((node[L], d+1))
         else:
             ns.append(node)
     return ns
 
 
-def augment_tree(sn):
+def annotate_tree(sn):
     i = 0
-    def augment_tree_rec(sn, depth, parent, side):
+    def go(sn, depth, parent, side):
         nonlocal i
         if list == type(sn):
-            augment_tree_rec(sn[0], depth+1, sn, L)
-            augment_tree_rec(sn[1], depth+1, sn, R)
+            go(sn[L], depth+1, sn, L)
+            go(sn[R], depth+1, sn, R)
         else:
-            parent[side] = (i, depth, sn)
+            parent[side] = {'id': i, 'depth': depth, 'value': sn, 'parent': parent, 'side': side}
             i += 1
-    augment_tree_rec(sn, 0, None, None)
+    go(sn, 0, None, None)
+
+
+def strip_tree(sn):
+    def go(sn, parent, side):
+        if list == type(sn):
+            go(sn[L], sn, L)
+            go(sn[R], sn, R)
+        else:
+            parent[side] = sn['value']
+    go(sn, None, None)
+
+
+def magnitude(tree):
+    if list == type(tree):
+        return 3*magnitude(tree[L]) + 2*magnitude(tree[R])
+    else:
+        if dict == type(tree):
+            return tree['value']
+        else:
+            return tree
 
 
 if __name__ == '__main__' and not sys.flags.interactive:
