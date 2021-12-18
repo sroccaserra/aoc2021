a48d40e5a4c243dd421c27db26c46bcac3a0a1dd
sroccaserra <sroccaserra@yahoo.com>
2021-12-18 09:09:17 +0100

    Inject ids and depth in nodes

--- src/18.py
+++ src/18.py
@@ -2,13 +2,20 @@ import sys
 import fileinput
 from collections import deque
 
+L = 0
+R = 1
+
+ID = 0
+DEPTH = 1
+VALUE = 2
 
 def solve_1(lines):
     sn = [[1,2],3]
     sn = [[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
     print(sn)
     print(create_map(sn))
-    return create_tree_dfs(sn)
+    augment_tree(sn)
+    return sn
 
 
 def create_map(sn):
@@ -23,50 +30,18 @@ def create_map(sn):
             ns.append(node)
     return ns
 
-ID = [0]
 
-def augment_tree(sn, t):
-    if list == type(sn):
-        augment_tree(sn[0], t)
-        augment_tree(sn[1], t)
-    else:
-        pass
-
-
-
-def create_tree_dfs(sn):
-    stack = [(sn, 0, 'l')]
+def augment_tree(sn):
     i = 0
-    root = []
-    current = root
-    left = None
-    right = None
-    levels = []
-    while stack:
-        node, d, side = stack.pop()
-        if type(node) == list:
-            levels.append(current)
-            new = []
-            current.append(new)
-            current = new
-            stack.append((node[1], d+1, 'r'))
-            stack.append((node[0], d+1, 'l'))
+    def augment_tree_rec(sn, depth, parent, side):
+        nonlocal i
+        if list == type(sn):
+            augment_tree_rec(sn[0], depth+1, sn, L)
+            augment_tree_rec(sn[1], depth+1, sn, R)
         else:
-            i = i+1
-            current.append(node)
-            if 'r' == side:
-                current = levels.pop()
-
-    return root[0]
-
-class Tree:
-    def __init__(self):
-        self.value = None
-        self.left = None
-        self.right = None
-
-    def is_leaf(self):
-        return self.value is not None
+            parent[side] = (i, depth, sn)
+            i += 1
+    augment_tree_rec(sn, 0, None, None)
 
 
 if __name__ == '__main__' and not sys.flags.interactive:
