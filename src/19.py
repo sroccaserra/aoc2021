36e8290e4d5f1cf2f6c0a174fd7bc417dd86ecad
sroccaserra <sroccaserra@yahoo.com>
2021-12-19 14:14:27 +0100

    :recycle: Remove useless memoisation

--- src/19.py
+++ src/19.py
@@ -10,14 +10,13 @@ def solve(scanners):
     to_visit = [scanners[0]]
     is_aligned[0] = True
     aligned = [scanners[0]]
-    memo = {}
     distances = []
     while not all(is_aligned):
         scanner = to_visit.pop()
         for i, other in enumerate(scanners):
             if is_aligned[i]:
                 continue
-            matching, distance = find_matching(memo, scanner, other)
+            matching, distance = find_matching(scanner, other)
             if matching is not None:
                 distances.append(distance)
                 aligned.append(matching)
@@ -38,9 +37,10 @@ def max_distance(distances):
             result = d
     return result
 
-def find_matching(memo, scanner, other):
-    for i, t in enumerate(TRANSFOS):
-        transformed = [transform(memo, i, p) for p in other]
+
+def find_matching(scanner, other):
+    for t in TRANSFOS:
+        transformed = [multiply(t, p) for p in other]
         distances = Counter()
         for (tx, ty, tz) in transformed:
             for (x, y, z) in scanner:
@@ -50,15 +50,6 @@ def find_matching(memo, scanner, other):
             dx, dy, dz = most_common_distance
             return set([(tx + dx, ty + dy, tz + dz) for (tx, ty, tz) in transformed]), (dx,dy,dz)
     return None, None
-
-
-def transform(memo, i, p):
-    if (i, p) in memo:
-        return memo[(i, p)]
-    t = TRANSFOS[i]
-    result = multiply(t, p)
-    memo[(i, p)] = result
-    return result
 
 
 TRANSFOS = [
@@ -90,6 +81,7 @@ TRANSFOS = [
         [[0, -1, 0],[0, 0, 1], [-1, 0, 0]],
         ]
 
+
 def multiply(m, p):
     result = [0, 0, 0]
     for i in range(3):
@@ -97,6 +89,7 @@ def multiply(m, p):
         for j in range(3):
             result[i] += r[j]*p[j]
     return tuple(result)
+
 
 if __name__ == '__main__' and not sys.flags.interactive:
     lines = [line.strip() for line in fileinput.input()]
