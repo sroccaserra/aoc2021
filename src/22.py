6fa89f561cd018a2838562750842e9de7ec4a80a
sroccaserra <sroccaserra@yahoo.com>
2021-12-22 13:09:13 +0100

    :recycle: Remove dead code

--- src/22.py
+++ src/22.py
@@ -5,32 +5,14 @@ from collections import defaultdict
 
 def solve_1(instructions):
     reactor = defaultdict(lambda:'off')
-    max_x = 0
-    min_x = 0
-    max_y = 0
-    min_y = 0
-    max_z = 0
-    min_z = 0
     for status, ranges in instructions:
         for x in range(ranges[0][0], ranges[0][1]+1):
-            if x > max_x:
-                max_x = x
-            elif x < min_x:
-                min_x = x
             if (x<-50) or (x > 50):
                 continue
             for y in range(ranges[1][0], ranges[1][1]+1):
-                if y > max_y:
-                    max_y = y
-                elif y < min_y:
-                    min_y = y
                 if (y<-50) or (y > 50):
                     continue
                 for z in range(ranges[2][0], ranges[2][1]+1):
-                    if z > max_z:
-                        max_z = z
-                    elif z < min_z:
-                        min_z = z
                     if (z<-50) or (z > 50):
                         continue
                     reactor[(x, y, z)] = status
@@ -39,17 +21,14 @@ def solve_1(instructions):
     for v in reactor.values():
         if v == 'on':
             result += 1
-    return result, (max_x-min_x)*(max_y-min_y)*(max_z-min_z)
+    return result
 
 
 def solve_2(instructions):
     weighted_rects = []
     for status, ranges in instructions:
-        rangex = ranges[0]
-        rangey = ranges[1]
-        rangez = ranges[2]
-        x1, y1, z1 = rangex[0], rangey[0], rangez[0]
-        x2, y2, z2 = rangex[1], rangey[1], rangez[1]
+        x1, y1, z1 = ranges[0][0], ranges[1][0], ranges[2][0]
+        x2, y2, z2 = ranges[0][1], ranges[1][1], ranges[2][1]
         rect = ((x1, y1, z1), (x2, y2, z2))
         weight = 1 if status == "on" else -1
         to_add = []
@@ -78,9 +57,9 @@ def solve_2(instructions):
 def volume(rect):
     (x1, y1, z1) = rect[0]
     (x2, y2, z2) = rect[1]
-    dx = (1+abs(x2-x1))
-    dy = (1+abs(y2-y1))
-    dz = (1+abs(z2-z1))
+    dx = 1 + abs(x2 - x1)
+    dy = 1 + abs(y2 - y1)
+    dz = 1 + abs(z2 - z1)
     return dx*dy*dz
 
 
@@ -90,7 +69,13 @@ def intersects(r1, r2):
     (r2x1, r2y1, r2z1) = r2[0]
     (r2x2, r2y2, r2z2) = r2[1]
 
-    return segments_intersect(r1x1, r1x2, r2x1, r2x2) and segments_intersect(r1y1, r1y2, r2y1, r2y2) and segments_intersect(r1z1, r1z2, r2z1, r2z2)
+    return segments_intersect(r1x1, r1x2, r2x1, r2x2) and \
+            segments_intersect(r1y1, r1y2, r2y1, r2y2) and \
+            segments_intersect(r1z1, r1z2, r2z1, r2z2)
+
+
+def segments_intersect(a1, a2, b1, b2):
+    return a2 >= b1 and b2 >= a1
 
 
 def common_rect(r1, r2):
@@ -103,12 +88,8 @@ def common_rect(r1, r2):
     return (topleftback, bottomrightfront)
 
 
-def segments_intersect(ax, bx, cx, dx):
-    return bx >= cx and dx >= ax
-
-
 if __name__ == '__main__' and not sys.flags.interactive:
     lines = [line.strip().split() for line in fileinput.input() if line[0] != '#']
     instructions = [(status, [[int(w) for w in r[2:].split('..')] for r in ranges.split(',')]) for (status, ranges) in lines]
-    # print(solve_1(instructions))
+    print(solve_1(instructions))
     print(solve_2(instructions))
