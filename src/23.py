42d115d5ec49c0dc5a05bacd97dd81f93b601479
sroccaserra <sroccaserra@yahoo.com>
2021-12-23 15:28:44 +0100

    🚧 [day 23] ⭕ Min search exceeds recursion depth

--- src/23.py
+++ src/23.py
@@ -17,13 +17,32 @@ def solve_1(filled_maze):
         print(r)
     state = find_starting_positions(filled_maze)
     maze = empty_maze(filled_maze)
-    return find_minimum_cost(maze, state)
+    dp = {}
+    return find_minimum_cost(dp, maze, state)
 
-def find_minimum_cost(maze, state):
+
+def find_minimum_cost(dp, maze, state):
+    key = tuple(sorted(state))
+    if key in dp:
+        return dp[key]
+    if len(dp)+1 % 100 == 0:
+        print(len(dp))
     moveable_amphipods = find_moveable_amphipods(state)
+    if len(moveable_amphipods) == 0:
+        dp[key] = 0
+        print('hey!')
+        return 0
+    possible_results = []
     for a in moveable_amphipods:
         possible_moves = find_possible_moves(maze, state, a)
-    return moveable_amphipods
+        if (len(possible_moves)) > 0:
+            print(possible_moves)
+        for move in possible_moves:
+            new_state = apply_move(state, move)
+            possible_results.append(energy_for(move)+find_minimum_cost(dp, maze, new_state))
+    result = min(possible_results)
+    dp[key] = result
+    return result
 
 
 def find_moveable_amphipods(state):
@@ -48,13 +67,24 @@ def find_moveable_amphipods(state):
 
     return result
 
+
 def find_possible_moves(maze, state, amphipod):
-    _, (xa, ya) = amphipod
+    letter, (xa, ya) = amphipod
     neighbors = [(xa+1, ya), (xa-1, ya), (xa, ya+1), (xa, ya-1)]
     non_walls = [(xn, yn) for xn, yn in neighbors if '.' == maze[yn][xn]]
-    non_occupied = [(xn, yn) for xn, yn in non_walls if not is_occupied(state, (xn, yn))]
-    return non_occupied
+    return [(letter, (xa, ya), (xn, yn)) for xn, yn in non_walls if not is_occupied(state, (xn, yn))]
 
+
+def apply_move(state, move):
+    letter, src, dst = move
+    result = [a for a in state if a != (letter, src)]
+    result.append((letter, dst))
+    return result
+
+
+def energy_for(move):
+    letter = move[0]
+    return ENERGY_FOR[letter]
 
 def other_same_pos(state, amphipod):
     (letter, pos) = amphipod
