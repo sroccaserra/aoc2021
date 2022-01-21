64d4f6c2fa08a92d6371e225000f2921a4cb1018
sroccaserra <sroccaserra@yahoo.com>
2022-01-21 17:00:40 +0100

    [day 23] ✨ Implement blocking bops in hallway

--- src/23.py
+++ src/23.py
@@ -18,7 +18,6 @@ DISTANCES = [
 ##
 # TODO
 
-# - Traiter le blocage dans le couloir
 # - Générer les mouvements possibles du couloir vers les chambres
 # - Faire la recherche de minimum
 
@@ -26,12 +25,19 @@ DISTANCES = [
 def solve_1(filled_maze):
     for r in filled_maze:
         print(r)
-    cost, state = start_cost_and_state(filled_maze)
     m = {}
+
+    cost, state = start_cost_and_state(filled_maze)
+    state_key = encode_state(state)
+    m[state_key] = cost
+
+    ns = neighbors_with_cost(state)
+    cost, state = ns[4]
     state_key = encode_state(state)
     m[state_key] = cost
     print(decode_state(state_key))
-    return neighbors_with_cost(state)
+    ns = neighbors_with_cost(state)
+    return len(ns), ns
 
 
 def neighbors_with_cost(state):
@@ -51,19 +57,62 @@ def apply_move(move, hallway, rooms):
 def possible_moves(hallway, rooms):
     return possible_moves_to_rooms(hallway, rooms) + possible_moves_to_hallway(hallway, rooms)
 
+
 def possible_moves_to_rooms(hallway, rooms):
     return []
 
+
 def possible_moves_to_hallway(hallway, rooms):
+    """
+    i: 01 2 3 4 56
+       .. . . . ..
+         a b c d
+    j:   0 1 2 3
+
+    i = 0, j = 0 => 01  range(0, 2) pivot = 2
+    i = 1, j = 0 => 1
+    i = 2, j = 0 => 2
+    i = 3, j = 0 => 23
+    i = 4, j = 0 => 234
+
+    i = 0, j = 1 => 012 range(0, 3) pivot = 3
+    i = 1, j = 1 => 12  range(1, 3)
+    i = 2, j = 1 => 2   range(2, 3)
+    i = 3, j = 1 => 3   range(3, 4)
+    i = 4, j = 1 => 34  range(3, 5)
+    i = 5, j = 1 => 345 range(3, 6)
+
+    i = 0, j = 2 => 0123 range(0, 4) pivot = 4
+
+    i = 0, j = 3 => 0123 range(0, 5) pivot = 5
+
+    pivot = j+2 !
+
+    if i < pivot
+    """
     result = []
     for dst_i in range(len(hallway)):
         c = hallway[dst_i]
-        if c != '.':
+        if c != FREE:
             continue
         for src_j in range(len(rooms)):
             room = rooms[src_j]
             if len(room) == 0:
                 continue
+
+            pivot = src_j + 2
+            if dst_i < pivot:
+                in_betweens = range(dst_i, pivot)
+            else:
+                in_betweens = range(pivot, dst_i+1)
+            is_reachable = True
+            for k in in_betweens:
+                if hallway[k] != FREE:
+                    is_reachable = False
+                    continue
+            if not is_reachable:
+                continue
+
             amphipod = room[-1]
             distance = DISTANCES[dst_i][src_j]
             cost = distance*ENERGY_FOR[amphipod]
