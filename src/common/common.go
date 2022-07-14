26c7f9c89fdf1534c5e997a2f92081b522a9f4b0
sroccaserra <sroccaserra@yahoo.com>
2022-07-14 17:18:53 +0200

    ♻ Move int parsing to common (Go)
    And play with Go generics a bit.

--- src/common/common.go
+++ src/common/common.go
@@ -4,10 +4,19 @@ import (
     "bufio"
     "io"
     "os"
+    "strconv"
 )
 
 func GetInputLines() []string {
-    var result []string
+    return GetParsedLines(id[string])
+}
+
+func GetInputInts() []int {
+    return GetParsedLines(parseInt)
+}
+
+func GetParsedLines[T any](parse func(string) T) []T {
+    var result []T
     var reader io.Reader
     if len(os.Args) > 1 {
         filename := os.Args[1]
@@ -17,7 +26,16 @@ func GetInputLines() []string {
     }
     scanner := bufio.NewScanner(reader)
     for scanner.Scan() {
-        result = append(result, scanner.Text())
+        result = append(result, parse(scanner.Text()))
     }
     return result
+}
+
+func id[T any](x T) T {
+    return x
+}
+
+func parseInt(s string) int {
+    n, _ := strconv.Atoi(s)
+    return n
 }
