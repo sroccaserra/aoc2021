187bd1a09e700dabda666e556ab1eba7c48941cf
sroccaserra <sroccaserra@yahoo.com>
2022-07-15 11:08:15 +0200

    🎨 Reformat Go files

--- src/01.go
+++ src/01.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 
-    "aoc2021/src/common"
+	"aoc2021/src/common"
 )
 
 const huge = 9999999999
 
 func solve_1(numbers []int) int {
-    previous := huge
-    result := 0
-    for _, n := range numbers {
-        if n > previous {
-            result += 1
-        }
-        previous = n
-    }
-    return result
+	previous := huge
+	result := 0
+	for _, n := range numbers {
+		if n > previous {
+			result += 1
+		}
+		previous = n
+	}
+	return result
 }
 
 func solve_2(numbers []int) int {
-    var p1, p2, p3 = huge, huge, huge
-    result := 0
-    for _, n := range numbers {
-        if n+p1+p2 > p1+p2+p3 {
-            result += 1
-        }
-        p1, p2, p3 = n, p1, p2
-    }
-    return result
+	var p1, p2, p3 = huge, huge, huge
+	result := 0
+	for _, n := range numbers {
+		if n+p1+p2 > p1+p2+p3 {
+			result += 1
+		}
+		p1, p2, p3 = n, p1, p2
+	}
+	return result
 }
 
 func main() {
-    numbers := common.GetInputInts()
-    fmt.Println(solve_1(numbers))
-    fmt.Println(solve_2(numbers))
+	numbers := common.GetInputInts()
+	fmt.Println(solve_1(numbers))
+	fmt.Println(solve_2(numbers))
 }
