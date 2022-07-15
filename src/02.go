0e3cf015fb7f6253396e5f0426f9900ad4248897
sroccaserra <sroccaserra@yahoo.com>
2022-07-15 11:08:15 +0200

    🎨 Reformat Go files

--- src/02.go
+++ src/02.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"strconv"
+	"strings"
 
-    "aoc2021/src/common"
+	"aoc2021/src/common"
 )
 
 func solve(commands []command) (int, int) {
-    var hpos, depth_1, depth_2, aim int
-    for _, command := range commands {
-        switch command.direction {
-        case "forward":
-            hpos += command.value
-            depth_2 += aim*command.value
-        case "up":
-            depth_1 -= command.value
-            aim -= command.value
-        case "down":
-            depth_1 += command.value
-            aim += command.value
-        }
-    }
-    return hpos*depth_1, hpos*depth_2
+	var hpos, depth_1, depth_2, aim int
+	for _, command := range commands {
+		switch command.direction {
+		case "forward":
+			hpos += command.value
+			depth_2 += aim * command.value
+		case "up":
+			depth_1 -= command.value
+			aim -= command.value
+		case "down":
+			depth_1 += command.value
+			aim += command.value
+		}
+	}
+	return hpos * depth_1, hpos * depth_2
 }
 
 func parse(line string) command {
-    parts := strings.Fields(line)
-    direction := parts[0]
-    value, _ := strconv.Atoi(parts[1])
-    return command{direction, value}
+	parts := strings.Fields(line)
+	direction := parts[0]
+	value, _ := strconv.Atoi(parts[1])
+	return command{direction, value}
 }
 
 type command struct {
-    direction string
-    value int
+	direction string
+	value     int
 }
 
 func main() {
-    commands := common.GetParsedLines(parse)
-    solution_1, solution_2 := solve(commands)
-    fmt.Println(solution_1)
-    fmt.Println(solution_2)
+	commands := common.GetParsedLines(parse)
+	solution_1, solution_2 := solve(commands)
+	fmt.Println(solution_1)
+	fmt.Println(solution_2)
 }
--- src/common/common.go
+++ src/common/common.go
@@ -1,41 +1,41 @@
 package common
 
 import (
-    "bufio"
-    "io"
-    "os"
-    "strconv"
+	"bufio"
+	"io"
+	"os"
+	"strconv"
 )
 
 func GetInputLines() []string {
-    return GetParsedLines(id[string])
+	return GetParsedLines(id[string])
 }
 
 func GetInputInts() []int {
-    return GetParsedLines(parseInt)
+	return GetParsedLines(parseInt)
 }
 
 func GetParsedLines[T any](parse func(string) T) []T {
-    var result []T
-    var reader io.Reader
-    if len(os.Args) > 1 {
-        filename := os.Args[1]
-        reader, _ = os.Open(filename)
-    } else {
-        reader = os.Stdin
-    }
-    scanner := bufio.NewScanner(reader)
-    for scanner.Scan() {
-        result = append(result, parse(scanner.Text()))
-    }
-    return result
+	var result []T
+	var reader io.Reader
+	if len(os.Args) > 1 {
+		filename := os.Args[1]
+		reader, _ = os.Open(filename)
+	} else {
+		reader = os.Stdin
+	}
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		result = append(result, parse(scanner.Text()))
+	}
+	return result
 }
 
 func id[T any](x T) T {
-    return x
+	return x
 }
 
 func parseInt(s string) int {
-    n, _ := strconv.Atoi(s)
-    return n
+	n, _ := strconv.Atoi(s)
+	return n
 }
