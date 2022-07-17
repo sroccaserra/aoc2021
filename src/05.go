ed229c5507fac6e1adcbd869ad01428bf3150467
sroccaserra <sroccaserra@yahoo.com>
2022-07-17 09:10:18 +0200

    ♻️ Use point struct as key (day 05, Go)

--- src/05.go
+++ src/05.go
@@ -7,7 +7,7 @@ import (
 )
 
 func solve_05_1(lines []line) int {
-	grid := map[string]int{}
+	grid := map[point]int{}
 	for _, line := range lines {
 		if line.x1 == line.x2 || line.y1 == line.y2 {
 			markLine(line, grid)
@@ -17,14 +17,14 @@ func solve_05_1(lines []line) int {
 }
 
 func solve_05_2(lines []line) int {
-	grid := map[string]int{}
+	grid := map[point]int{}
 	for _, line := range lines {
 		markLine(line, grid)
 	}
 	return countResult(grid)
 }
 
-func markLine(line line, grid map[string]int) {
+func markLine(line line, grid map[point]int) {
 	deltaX, deltaY := line.x2-line.x1, line.y2-line.y1
 	var dx, dy int
 	switch {
@@ -45,26 +45,22 @@ func markLine(line line, grid map[string]int) {
 	}
 	n := Max(Abs(deltaX), Abs(deltaY))
 	for x, y, i := line.x1, line.y1, 0; i <= n; x, y, i = x+dx, y+dy, i+1 {
-		key := makeKey(x, y)
-		grid[key] += 1
+		grid[point{x, y}]++
 	}
 }
 
-func countResult(grid map[string]int) int {
+func countResult(grid map[point]int) int {
 	var result int
 	for _, v := range grid {
 		if v > 1 {
-			result += 1
+			result++
 		}
 	}
 	return result
 }
 
 type line struct{ x1, y1, x2, y2 int }
-
-func makeKey(x int, y int) string {
-	return fmt.Sprintf("%d,%d", x, y)
-}
+type point struct{ x, y int }
 
 func parse_05(s string) line {
 	var x1, y1, x2, y2 int
