c24a4c2fb3855c0a4a917afc4b4021553d955749
sroccaserra <sroccaserra@yahoo.com>
2022-07-23 01:25:26 +0200

    ✨ [day 01] Solve part two (Forth)

--- src/01.fs
+++ src/01.fs
@@ -17,6 +17,33 @@ Create numbers max-lines cells allot
         swap drop ( count n )
     loop drop ( count ) ;
 
+: sum-two ( addr -- sum )
+    dup @ swap cell+ @ + ;
+
+: sum-three ( addr -- sum )
+    dup sum-two swap 2 cells + @ + ;
+
+: has-bigger-sum ( addr-prevs n -- flag )
+    swap dup sum-three ( n addr-prevs prevs-sum )
+    swap sum-two rot +  ( prevs-sum new-sum )
+    < ;
+
+: shift-previous ( addr-prevs n -- addr-prevs )
+    swap dup cell+ @ over 2 cells + !
+    dup @ over cell+ !
+    dup -rot ! ;
+
+: solve-01-2 ( -- result )
+    0 here huge , huge , huge , ( count addr-prevs )
+    nb-lines 0 do
+        numbers i cells + @ ( count addr-prevs n )
+        2dup has-bigger-sum
+        if
+            rot 1+ -rot ( count+1 addr-prevs n )
+        then
+        shift-previous ( count addr-prevs )
+    loop drop ( count ) ;
+
 : parse-lines
     next-arg r/o open-file throw >r
 
@@ -32,4 +59,5 @@ Create numbers max-lines cells allot
 
 parse-lines
 solve-01-1 . cr
+solve-01-2 . cr
 bye
