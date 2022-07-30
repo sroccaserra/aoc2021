f229035e701d369e55a3dbf4b221414a91b3af19
sroccaserra <sroccaserra@yahoo.com>
2022-07-30 21:01:26 +0200

    ♻️ Simplify day 01 solutions (Python)

--- src/01.py
+++ src/01.py
@@ -1,33 +1,25 @@
 import sys
 import fileinput
 
-from collections import deque
 
 def solve_1(numbers):
-    other_lines = numbers.copy()
-    n = 0
-    for i in range(len(numbers)-1):
-        if other_lines[i+1] > numbers[i]:
-            n += 1
-    return n
+    result = 0
+    for i in range(len(numbers) - 1):
+        if numbers[i+1] > numbers[i]:
+            result += 1
+    return result
+
 
 def solve_2(numbers):
-    ns_1 = deque(numbers)
-    ns_1.pop()
-    ns_1.pop()
-    ns_2 = deque(numbers)
-    ns_2.popleft()
-    ns_2.pop()
-    ns_3 = deque(numbers)
-    ns_3.popleft()
-    ns_3.popleft()
-    n = 0
-    for i in range(len(ns_1)-1):
-        s1 = ns_1[i]+ ns_2[i]+ ns_3[i]
-        s2 = ns_1[i+1]+ ns_2[i+1]+ ns_3[i+1]
-        if s2 > s1:
-            n += 1
-    return n
+    HUGE = 9999999999
+    p1, p2, p3 = HUGE, HUGE, HUGE
+    result = 0
+    for n in numbers:
+        if n + p1 + p2 > p1 + p2 + p3:
+            result += 1
+        p1, p2, p3 = n, p1, p2
+    return result
+
 
 if __name__ == '__main__' and not sys.flags.interactive:
     numbers = [int(line.strip()) for line in fileinput.input()]
