6235c2bebfd0f3a93f904a6646cea191fc2b5414
sroccaserra <sroccaserra@yahoo.com>
2022-07-30 22:35:37 +0200

    ♻️ Try a local variable for the submarine (Forth)

--- src/02.fs
+++ src/02.fs
@@ -4,15 +4,7 @@ require common/common.fs
 1000 Constant max-lines
 Create numbers max-lines cells allot
 
-: command.direction@ ( command-addr -- c )
-    c@ ;
-
-: command.value@ ( command-addr -- n )
-    char+ @ ;
-
-: create-submarine ( -- sub-addr )
-    here 0 , 0 , 0 , 0 , ;
-
+: create-submarine ( -- sub-addr ) here 0 , 0 , 0 , 0 , ;
 : sub.hpos ( sub-addr -- addr ) ;
 : sub.depth-1 ( sub-addr -- addr ) cell+ ;
 : sub.depth-2 ( sub-addr -- addr ) 2 cells + ;
@@ -20,47 +12,38 @@ Create numbers max-lines cells allot
 
 : inc-sub-hpos ( sub-addr n -- )
     over sub.hpos @ + swap sub.hpos ! ;
-
 : inc-sub-depth-1 ( sub-addr n -- )
     over sub.depth-1 @ + swap sub.depth-1 ! ;
-
 : dec-sub-depth-1 ( sub-addr n  -- )
     negate inc-sub-depth-1 ;
-
 : inc-sub-depth-2 ( sub-addr n -- )
     over sub.depth-2 @ + swap sub.depth-2 ! ;
-
 : dec-sub-depth-2 ( sub-addr n  -- )
     negate inc-sub-depth-2 ;
-
 : inc-sub-aim ( sub-addr n -- )
     over sub.aim @ + swap sub.aim ! ;
-
 : dec-sub-aim ( sub-addr n  -- )
     negate inc-sub-aim ;
 
-: eval-command ( command-addr sub-addr -- )
-    swap dup command.value@ swap command.direction@
+: command.direction@ ( command-addr -- c ) c@ ;
+: command.value@ ( command-addr -- n ) char+ @ ;
+
+: eval-command ( sub-addr command-addr -- )
+    dup command.value@ swap command.direction@
     case ( addr n )
         'f' of 2dup inc-sub-hpos over sub.aim @ * inc-sub-depth-2 endof
         'u' of 2dup dec-sub-depth-1 dec-sub-aim endof
         'd' of 2dup inc-sub-depth-1 inc-sub-aim endof
     endcase ;
 
-: eval-result-1 ( sub-addr -- n )
-    dup sub.hpos @ swap sub.depth-1 @ * ;
-
-: eval-result-2 ( sub-addr -- n )
-    dup sub.hpos @ swap sub.depth-2 @ * ;
-
 : solve-02 ( -- result-2 result-1 )
     assert( nb-lines max-lines <= )
-    create-submarine >r
-    numbers nb-lines cells bounds begin
-        dup @ r@ eval-command
-        cell+ 2dup <=
-    until 2drop
-    r> dup eval-result-2 swap eval-result-1 ;
+    create-submarine { submarine }
+    numbers nb-lines cells bounds do
+        submarine i @ eval-command
+    cell +loop
+    submarine dup sub.hpos @ swap sub.depth-2 @ *
+    submarine dup sub.hpos @ swap sub.depth-1 @ * ;
 
 : parse-02 ( c-addr u -- command-addr )
     here >r
