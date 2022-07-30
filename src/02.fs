808f7d21c9979260b485bd8583a5ebf8f3be8294
sroccaserra <sroccaserra@yahoo.com>
2022-07-30 22:07:36 +0200

    ♻️ Try some accessor look-alikes (Forth)

--- src/02.fs
+++ src/02.fs
@@ -4,61 +4,54 @@ require common/common.fs
 1000 Constant max-lines
 Create numbers max-lines cells allot
 
-: command-direction ( command-addr -- c )
+: command.direction@ ( command-addr -- c )
     c@ ;
 
-: command-value ( command-addr -- n )
+: command.value@ ( command-addr -- n )
     char+ @ ;
 
 : create-submarine ( -- sub-addr )
     here 0 , 0 , 0 , 0 , ;
 
-: sub-hpos ( sub-addr -- n )
-    @ ;
+: sub.hpos ( sub-addr -- addr ) ;
+: sub.depth-1 ( sub-addr -- addr ) cell+ ;
+: sub.depth-2 ( sub-addr -- addr ) 2 cells + ;
+: sub.aim ( sub-addr -- addr ) 3 cells + ;
 
 : inc-sub-hpos ( sub-addr n -- )
-    over sub-hpos + swap ! ;
-
-: sub-depth-1 ( sub-addr -- n )
-    cell+ @ ;
+    over sub.hpos @ + swap sub.hpos ! ;
 
 : inc-sub-depth-1 ( sub-addr n -- )
-    over sub-depth-1 + swap cell+ ! ;
+    over sub.depth-1 @ + swap sub.depth-1 ! ;
 
 : dec-sub-depth-1 ( sub-addr n  -- )
     negate inc-sub-depth-1 ;
 
-: sub-depth-2 ( sub-addr -- n )
-    2 cells + @ ;
-
 : inc-sub-depth-2 ( sub-addr n -- )
-    over sub-depth-2 + swap 2 cells + ! ;
+    over sub.depth-2 @ + swap sub.depth-2 ! ;
 
 : dec-sub-depth-2 ( sub-addr n  -- )
     negate inc-sub-depth-2 ;
 
-: sub-aim ( sub-addr -- n )
-    3 cells + @ ;
-
 : inc-sub-aim ( sub-addr n -- )
-    over sub-aim + swap 3 cells + ! ;
+    over sub.aim @ + swap sub.aim ! ;
 
 : dec-sub-aim ( sub-addr n  -- )
     negate inc-sub-aim ;
 
 : eval-command ( command-addr sub-addr -- )
-    swap dup command-value swap command-direction
+    swap dup command.value@ swap command.direction@
     case ( addr n )
-        'f' of 2dup inc-sub-hpos over sub-aim * inc-sub-depth-2 endof
+        'f' of 2dup inc-sub-hpos over sub.aim @ * inc-sub-depth-2 endof
         'u' of 2dup dec-sub-depth-1 dec-sub-aim endof
         'd' of 2dup inc-sub-depth-1 inc-sub-aim endof
     endcase ;
 
 : eval-result-1 ( sub-addr -- n )
-    dup sub-hpos swap sub-depth-1 * ;
+    dup sub.hpos @ swap sub.depth-1 @ * ;
 
 : eval-result-2 ( sub-addr -- n )
-    dup sub-hpos swap sub-depth-2 * ;
+    dup sub.hpos @ swap sub.depth-2 @ * ;
 
 : solve-02 ( -- result-2 result-1 )
     assert( nb-lines max-lines <= )
