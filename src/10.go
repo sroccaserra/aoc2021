f6429a0ff706ad6194aa0ef4c9806eab3995f2e8
sroccaserra <sroccaserra@yahoo.com>
2022-08-25 16:14:53 +0200

    ✨ [day 10] Solve part two (Go)

--- src/10.go
+++ src/10.go
@@ -2,51 +2,102 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	. "aoc2021/src/common"
 )
 
-func solve_10_1(lines []string) int {
-	result := 0
+func solve_10(lines []string) (int, int) {
+	result_1 := 0
+	var scores_2 []int
 	for _, line := range lines {
 		if firstIllegal, isCorrupted := isCorrupted(line); isCorrupted {
-			result += pointsFor[firstIllegal]
+			result_1 += pointsForCorrupted[firstIllegal]
+		} else {
+			neededClosingChars := neededClosingChars(line)
+			score := scoreForClosingChars(neededClosingChars)
+			scores_2 = append(scores_2, score)
 		}
 	}
-	return result
+	middleIndex := len(scores_2) / 2
+	sort.Ints(scores_2)
+	return result_1, scores_2[middleIndex]
 }
 
 func isCorrupted(line string) (rune, bool) {
 	var openingStack []rune
 	for _, c := range line {
-		openingChar, ok := requiredOpeningChar[c]
-		if !ok {
-			Push(&openingStack, c)
-		} else {
+		matchingOpeningChar, isClosingChar := matchingOpeningCharFor[c]
+		if isClosingChar {
 			previous := Pop(&openingStack)
-			if previous != openingChar {
+			if previous != matchingOpeningChar {
 				return c, true
+			}
+		} else {
+			Push(&openingStack, c)
+		}
+	}
+	return '?', false
+}
+
+func neededClosingChars(line string) []rune {
+	var result []rune
+	for _, c := range line {
+		matchingClosingChar, isOpeningChar := matchingClosingCharFor[c]
+		if isOpeningChar {
+			Push(&result, matchingClosingChar)
+		} else {
+			lastIndex := len(result) - 1
+			if result[lastIndex] == c {
+				Pop(&result)
 			}
 		}
 	}
-	return 'x', false
+	return result
 }
 
-var requiredOpeningChar = map[rune]rune{
+func scoreForClosingChars(closingChars []rune) int {
+	result := 0
+	lastIndex := len(closingChars) - 1
+	for i := range closingChars {
+		c := closingChars[lastIndex-i]
+		result *= 5
+		result += pointsForIncomplete[c]
+	}
+	return result
+}
+
+var matchingOpeningCharFor = map[rune]rune{
 	')': '(',
 	']': '[',
 	'}': '{',
 	'>': '<',
 }
 
-var pointsFor = map[rune]int{
+var matchingClosingCharFor = map[rune]rune{
+	'(': ')',
+	'[': ']',
+	'{': '}',
+	'<': '>',
+}
+
+var pointsForCorrupted = map[rune]int{
 	')': 3,
 	']': 57,
 	'}': 1197,
 	'>': 25137,
 }
 
+var pointsForIncomplete = map[rune]int{
+	')': 1,
+	']': 2,
+	'}': 3,
+	'>': 4,
+}
+
 func main() {
 	lines := GetInputLines()
-	fmt.Println(solve_10_1(lines))
+	result_1, result_2 := solve_10(lines)
+	fmt.Println(result_1)
+	fmt.Println(result_2)
 }
