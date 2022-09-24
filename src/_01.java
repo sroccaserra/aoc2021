80b3881fe78e21cc5cfaac778e503b749345bf7e
sroccaserra <sroccaserra@yahoo.com>
2022-09-24 09:23:04 +0200

    ♻️ Remove intermediate class in Java solutions

--- src/_01.java
+++ src/_01.java
@@ -6,47 +6,43 @@ import static java.lang.String.format;
 import aoc.common.InputReader;
 import aoc.common.Solver;
 
-public class _01
-{
-    private final static class Day01Solver implements Solver
-    {
-        private final List<Integer> numbers = new ArrayList<>();
-        private final static Integer HUGE = 99999;
+public class _01 implements Solver {
+    private final List<Integer> numbers = new ArrayList<>();
+    private final static Integer HUGE = 99999;
 
-        Integer solve1() {
-            var result = 0;
-            var previous = HUGE;
-            for (final var n : numbers) {
-                if (n > previous) {
-                    ++result;
-                }
-                previous = n;
+    Integer solve1() {
+        var result = 0;
+        var previous = HUGE;
+        for (final var n : numbers) {
+            if (n > previous) {
+                ++result;
             }
-            return result;
+            previous = n;
         }
+        return result;
+    }
 
-        Integer solve2() {
-            var result = 0;
-            Integer p1 = HUGE, p2 = HUGE, p3 = HUGE;
-            for (final var n : numbers) {
-                if (n + p1 + p2 > p1 + p2 + p3) {
-                    ++result;
-                }
-                p3 = p2;
-                p2 = p1;
-                p1 = n;
+    Integer solve2() {
+        var result = 0;
+        Integer p1 = HUGE, p2 = HUGE, p3 = HUGE;
+        for (final var n : numbers) {
+            if (n + p1 + p2 > p1 + p2 + p3) {
+                ++result;
             }
-            return result;
+            p3 = p2;
+            p2 = p1;
+            p1 = n;
         }
+        return result;
+    }
 
-        @Override
-        public void processLine(String line) {
-            numbers.add(Integer.valueOf(line));
-        }
+    @Override
+    public void processLine(String line) {
+        numbers.add(Integer.valueOf(line));
     }
 
     public static void main(String[] args) {
-        final var solver = new Day01Solver();
+        final var solver = new _01();
         InputReader.readLines(args[0], solver);
         System.out.println(format("%d", solver.solve1()));
         System.out.println(format("%d", solver.solve2()));
--- src/_02.java
+++ src/_02.java
@@ -4,51 +4,47 @@ import java.util.ArrayList;
 import aoc.common.InputReader;
 import aoc.common.Solver;
 
-public class _02
-{
-    private final static class Day02Solver implements Solver
-    {
-        private final List<Command> commands = new ArrayList<>();
+public class _02 implements Solver {
+    private final List<Command> commands = new ArrayList<>();
 
-        Results solve() {
-            var hpos = 0;
-            var depth_1 = 0;
-            var depth_2 = 0;
-            var aim = 0;
-            for (final var command : commands) {
-                switch (command.direction) {
-                    case "forward":
-                        hpos += command.value;
-                        depth_2 +=  aim * command.value;
-                        break;
-                    case "up":
-                        depth_1 -= command.value;
-                        aim -=  command.value;
-                        break;
-                    case "down":
-                        depth_1 += command.value;
-                        aim +=  command.value;
-                        break;
-                }
+    Results solve() {
+        var hpos = 0;
+        var depth_1 = 0;
+        var depth_2 = 0;
+        var aim = 0;
+        for (final var command : commands) {
+            switch (command.direction) {
+                case "forward":
+                    hpos += command.value;
+                    depth_2 +=  aim * command.value;
+                    break;
+                case "up":
+                    depth_1 -= command.value;
+                    aim -=  command.value;
+                    break;
+                case "down":
+                    depth_1 += command.value;
+                    aim +=  command.value;
+                    break;
             }
-
-            return new Results(hpos * depth_1, hpos * depth_2);
         }
 
-        @Override
-        public void processLine(String line) {
-            final String[] parts = line.split(" ");
-            commands.add(new Command(parts[0], Integer.valueOf(parts[1])));
-        }
+        return new Results(hpos * depth_1, hpos * depth_2);
+    }
+
+    @Override
+    public void processLine(String line) {
+        final String[] parts = line.split(" ");
+        commands.add(new Command(parts[0], Integer.valueOf(parts[1])));
     }
 
     private static record Command(String direction, Integer value) {}
     private static record Results(Integer first, Integer second) {}
 
     public static void main(String[] args) {
-        final var solver = new Day02Solver();
+        final var solver = new _02();
         InputReader.readLines(args[0], solver);
-        final Results results = solver.solve();
+        final var results = solver.solve();
         System.out.println(results.first);
         System.out.println(results.second);
     }
