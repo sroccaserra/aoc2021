409e464da6d4aa04ad3460afceb32fa8efa56432
sroccaserra <sroccaserra@yahoo.com>
2022-10-16 00:09:36 +0200

    ♻️ Use atoms in tuples (Erlang)

--- src/02.erl
+++ src/02.erl
@@ -8,10 +8,9 @@ solve(Commands) ->
     solve(Commands, 0, 0, 0, 0).
 
 solve([], HPos, Depth1, Depth2, _) ->
-    {HPos * Depth1, HPos * Depth2};
+    {results, HPos * Depth1, HPos * Depth2};
 
-solve([Command | Rest], HPos, Depth1, Depth2, Aim) ->
-    {Direction, Value} = Command,
+solve([{command, Direction, Value} | Rest], HPos, Depth1, Depth2, Aim) ->
     case Direction of
         forward -> solve(Rest, HPos+Value, Depth1, Depth2+Aim*Value, Aim);
         up -> solve(Rest, HPos, Depth1-Value, Depth2, Aim-Value);
@@ -20,9 +19,9 @@ solve([Command | Rest], HPos, Depth1, Depth2, Aim) ->
 
 parse_command(Line) ->
     [Part1 | [Part2 | _]] = string:tokens(Line, [$\s]),
-    {list_to_atom(Part1), list_to_integer(Part2)}.
+    {command, list_to_atom(Part1), list_to_integer(Part2)}.
 
 main([Filename]) ->
     Commands = get_parsed_lines(Filename, fun parse_command/1),
-    {Result1, Result2} = solve(Commands),
+    {results, Result1, Result2} = solve(Commands),
     io:fwrite("~w~n~w~n", [Result1, Result2]).
