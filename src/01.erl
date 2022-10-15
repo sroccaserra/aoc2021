cdac9b40b6abae3462970f4ddd24eb6f3eb0a12a
sroccaserra <sroccaserra@yahoo.com>
2022-10-15 22:42:45 +0200

    ✨ [day 01] Solve part two (Erlang)

--- src/01.erl
+++ src/01.erl
@@ -13,9 +13,20 @@ solve_1([N|Rest], Previous, Result) ->
         _ -> solve_1(Rest, N, NextResult)
     end.
 
+solve_2(Numbers) ->
+    solve_2(Numbers, ?HUGE, ?HUGE, ?HUGE, 0).
+
+solve_2([N|Rest], P1, P2, P3, Result) ->
+    NextResult = Result + case (N+P1+P2 > P1+P2+P3) of true -> 1; false -> 0 end,
+    case Rest of
+        [] -> NextResult;
+        _ -> solve_2(Rest, N, P1, P2, NextResult)
+    end.
+
 main([Filename]) ->
     Numbers = get_parsed_lines(Filename, fun erlang:list_to_integer/1),
-    io:fwrite("~w~n", [solve_1(Numbers)]).
+    io:fwrite("~w~n", [solve_1(Numbers)]),
+    io:fwrite("~w~n", [solve_2(Numbers)]).
 
 %%
 % IO Functions
