887616d4f271c8b7381aac497d35f0782e130012
sroccaserra <sroccaserra@yahoo.com>
2022-10-16 13:53:48 +0200

    ♻️ Use more pattern matching in day 01 (Erlang)

--- src/01.erl
+++ src/01.erl
@@ -7,21 +7,25 @@
 solve_1(Numbers) ->
     solve_1(Numbers, ?HUGE, 0).
 
+solve_1([], _, Result) ->
+    Result;
+
 solve_1([N | Rest], Previous, Result) ->
-    NextResult = Result + case (N > Previous) of true -> 1; false -> 0 end,
-    case Rest of
-        [] -> NextResult;
-        _ -> solve_1(Rest, N, NextResult)
+    case N > Previous of
+        true -> solve_1(Rest, N, Result + 1);
+        false -> solve_1(Rest, N, Result)
     end.
 
 solve_2(Numbers) ->
     solve_2(Numbers, ?HUGE, ?HUGE, ?HUGE, 0).
 
+solve_2([], _, _, _, Result) ->
+    Result;
+
 solve_2([N | Rest], P1, P2, P3, Result) ->
-    NextResult = Result + case (N+P1+P2 > P1+P2+P3) of true -> 1; false -> 0 end,
-    case Rest of
-        [] -> NextResult;
-        _ -> solve_2(Rest, N, P1, P2, NextResult)
+    case (N+P1+P2 > P1+P2+P3) of
+        true -> solve_2(Rest, N, P1, P2, Result + 1);
+        false -> solve_2(Rest, N, P1, P2, Result)
     end.
 
 main([Filename]) ->
