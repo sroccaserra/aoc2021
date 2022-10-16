20b0209255132ca2618aa69ece22b0282a85fde5
sroccaserra <sroccaserra@yahoo.com>
2022-10-16 13:47:20 +0200

    ✨ [day 01] Add more similar solution (Elixir)

--- src/01.exs
+++ src/01.exs
@@ -3,6 +3,27 @@ import IO, only: [puts: 1, read: 2]
 import String, only: [split: 3, to_integer: 1]
 
 defmodule Day01 do
+  @huge 9999
+
+  ##
+  # The "solve_1" solution is more in line with most other languages solutions
+  def solve_1(numbers) do
+    solve_1(numbers, @huge, 0)
+  end
+
+  def solve_1([], _, result) do
+    result
+  end
+
+  def solve_1([n|rest], previous, result) do
+    case n > previous do
+      true -> solve_1(rest, n, result + 1)
+      false -> solve_1(rest, n, result)
+    end
+  end
+
+  ##
+  # Here, the "part_1" solutions are more in line with the Haskell solutions
   def part_1(numbers) do
     zip(numbers, tl(numbers))
     |> count(fn {left, right} -> left < right end)
@@ -25,5 +46,5 @@ numbers = read(:stdio, :all)
           |> split("\n", trim: true)
           |> map(&to_integer/1)
 
-puts Day01.part_1(numbers)
+puts Day01.solve_1(numbers)
 puts Day01.part_2(numbers)
