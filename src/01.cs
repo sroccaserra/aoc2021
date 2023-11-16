1aeea6a3f02e2977bbdede51475d891a5a4d8e5a
Sébastien Roccaserra <sroccaserra@yahoo.com>
2023-11-16 16:52:32 +0100

    f [day 01] Solve part two (C#)

--- src/01.cs
+++ src/01.cs
@@ -1,10 +1,16 @@
-int Solve1(IEnumerable<int> numbers)
-{
+int Solve1(IEnumerable<int> numbers) {
     return (
             from pair in numbers.Zip(numbers.Skip(1))
             where pair.First < pair.Second
             select 1
         ).Count();
+}
+
+int Solve2(IEnumerable<int> numbers) {
+	var by3 = from tuple in Enumerable.Zip(numbers, numbers.Skip(1), numbers.Skip(2))
+		select tuple.First + tuple.Second + tuple.Third;
+	
+	return Solve1(by3);
 }
 
 var input = File.ReadAllText("src/01.in");
@@ -13,3 +19,4 @@ var numbers = from line in input.Split('\n')
     select int.Parse(line);
 
 Console.WriteLine(Solve1(numbers));
+Console.WriteLine(Solve2(numbers));
